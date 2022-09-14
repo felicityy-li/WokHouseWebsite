4ed62b58e224b33507d6f37cda57b22b412a7b63
felicityy-li <88757077+felicityy-li@users.noreply.github.com>
2022-09-14 10:57:13 -0400

    chore: set up frontend system

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Menu from "./pages/Menu";
+import ContactUs from "./pages/ContactUs";
+import AboutUs from "./pages/AboutUs";
+import OrderOnlineSystem from "./pages/OrderOnlineSystem";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/menu_nonorders" element={<Menu />} />
+          <Route path="/contact_us" element={<ContactUs />} />
+          <Route path="/about_us" element={<AboutUs />} />
+          <Route path="/order_online" element={<OrderOnlineSystem />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/OrderOnlineSystem.js
@@ -0,0 +1,9 @@
+import Menu from "./Menu";
+
+export default function OrderOnlineSystem() {
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+}
